238e92210b295f05d78580682c749cabd858f656
Rhuan Marques <magicknight@hotmail.com.br>
2020-12-07 20:35:44 -0300

    create the chat table
    Co-authored-by: Poulvilho <pv.paulovictor.vp@gmail.com>
    Co-authored-by: Denniel William <dennielwilliam.unb@gmail.com>
    Co-authored-by: Edvan Barreira <edvan.bgjunior@gmail.com>

---
+++ src/controllers/messageController.js
@@ -0,0 +1,93 @@
+require('dotenv/config');
+
+const { v4: uuidv4 } = require('uuid');
+const bcrypt = require('bcrypt');
+
+const Mensagem = require('../models/Message')
+
+module.exports = {
+
+  async create(request, response) {
+    const id = uuidv4();
+    const {
+      text,
+      classroom_id,
+      student_id,
+      teacher_id, 
+      create_by 
+    } = request.body;
+
+    try {
+      const mensagem = await Mensagem.create({
+        id,
+        text,
+        classroom_id,
+        student_id,
+        teacher_id, 
+        create_by
+      });
+
+      if (!mensagem) {
+        return response.status(200).json({
+          message: 'Erro ao criar mensagem!'
+        });
+      }
+      return response.status(200).json({
+        data: {
+          mensagem
+        },
+        message: 'mensagem criada com sucesso!'
+      });
+    } catch (error) {
+      console.log(error);
+      return response.status(400).json({
+        message: error
+      });
+    }
+  },
+
+  async read(request, response) {
+    const { id } = request.params;
+
+    try {
+      const mensagem = await Mensagem.findByPk(id);
+
+      if (!mensagem) {
+        return response.status(200).json({
+          message: 'Mensagem não encontrada!'
+        });
+      }
+      return response.status(200).json({
+        data: {
+          mensagem
+        },
+        message: 'Mensagem encontrado com sucesso'
+      });
+    } catch (error) {
+      console.log(error);
+      return response.status(200).json({
+        message: error
+      });
+    }
+  },
+
+  async chat(request, response) {
+    const { classroom_id } = request.params;
+    try {   
+      const mensagem = await Mensagem.findAll({
+        where: { classroom_id }
+      });
+      return response.status(200).json({
+        data: {
+          mensagem
+        }
+      });
+    } catch (error) {
+      console.log(error);
+      return response.status(400).json({
+        message: error
+      });
+    }
+  },
+
+};
